fcd6dfca9eaa3699cf630dce54e73c7cd7699eca
jong-jang <jjh0299@gmail.com>
2021-08-12 10:24:54 +0900

    스크롤 투명도 애니메이션 추가

--- js/src/clone1.js
+++ js/src/clone1.js
@@ -18,6 +18,12 @@ var otList = [con1, con2, con3 ,con4, con5, con6, con7, con8, con9];
 var winH = screen.height;
 var con7T = con7.getElementsByClassName('con_07_top')[0];
 var con7Td = con7T.getElementsByTagName('div')[0];
+var con7M = con7.getElementsByClassName('con_07_mid')[0];
+var con7Md1 = con7M.getElementsByTagName('div')[0];
+var con7Md2 = con7M.getElementsByTagName('div')[1];
+var con7B = con7.getElementsByClassName('con_07_bot')[0];
+var con7Bd = con7B.getElementsByTagName('div')[0];
+var conList = [con7Td, con7Md1, con7Md2, con7Bd];
 // ----------------------
 const moveScrollFn = (otdata) => {
   var scTop = document.documentElement.scrollTop;
@@ -32,28 +38,46 @@ const moveScrollFn = (otdata) => {
 
   return moveScr;
 }
+const pFn = (pset) => {
+  let pt, pv, st;
+  st = document.documentElement.scrollTop;
+  pt = (st - pset.offsetTop)/1000;
+  if(pt > 0){
+    pt = 0;
+  }
+  console.log(pt);
+  pv = 1 + pt;
 
+  return pv;
+}
 const scrollFn = () => {
-  let otLists, otone, moveline, pd;
-  let st = document.documentElement.scrollTop
+  let otLists, otone, moveline, pd, pv;
+  let st = document.documentElement.scrollTop;
   pd = st - con6.offsetTop + winH/2;
   let i=0;
-  for(; i<otList.length; i++){
-    otLists = otList[i].offsetTop;
+  let j=0;
+  for(; j<otList.length; j++){
+    otLists = otList[j].offsetTop;
     moveline = moveScrollFn(otLists);
     if(moveline >= 30){
-      otList[i].classList.add('act');
+      otList[j].classList.add('act');
     }
   }
   if(pd > 0){
     con6.parentElement.style.padding = 0+' '+(350-pd)+'px';
+  }
+  for(; i<conList.length; i++){
+    pv = pFn(conList[i]);
+
+    if(pv > 0){
+      conList[i].style.opacity = pv;
+    }
   }
 }
 
 // 이벤트
 document.addEventListener('scroll', function(){
   scrollFn();
-  console.log(con6.offsetTop);
 })
 
 moB.addEventListener('click', function(event){
